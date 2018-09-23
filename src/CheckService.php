e691fd6f199dc3e6548d9dffb7134059bde71179
Mohamed Nagy <mohamed.n.haleem@gmail.com>
2018-09-23 22:38:20 +0200

    formatting

--- src/CheckService.php
+++ src/CheckService.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Nagy\HealthChecker;
 
 use Exception;
@@ -7,56 +8,56 @@ use Nagy\HealthChecker\Events\HealthEvent;
 
 class CheckService
 {
-	private $checks = [];
+    private $checks = [];
 
-	public function __construct(array $checks = null)
-	{
-	    $checks = $checks ?? $this->getCheckers();
-		$this->checks = $checks;
-	}
+    public function __construct(array $checks = null)
+    {
+        $checks = $checks ?? $this->getCheckers();
+        $this->checks = $checks;
+    }
 
-	public function runAll(): Collection
-	{
-		$result = collect();
-		foreach ($this->checks as $name => $check) {
-			$result->push($this->run($name, $check));
-		}
+    public function runAll(): Collection
+    {
+        $result = collect();
+        foreach ($this->checks as $name => $check) {
+            $result->push($this->run($name, $check));
+        }
 
-		event(new HealthEvent($result));
+        event(new HealthEvent($result));
 
-		return $result;
-	}
+        return $result;
+    }
 
-	public function run(string $name, array $checker = []): Result
-	{
-		try {
-		    if (count($checker) == 0) {
-		        $checker = $this->getCheckerByName($name);
+    public function run(string $name, array $checker = []): Result
+    {
+        try {
+            if (count($checker) == 0) {
+                $checker = $this->getCheckerByName($name);
             }
 
             $options = $checker['options'];
-			$options['checkerName'] = $name;
+            $options['checkerName'] = $name;
 
-			$checker = new $checker['class']($options);
+            $checker = new $checker['class']($options);
 
-			$time = microtime(true);
-			$result = $checker->check();
-			$time = microtime(true) - $time;
-			$result->setExecutionTime($time);
+            $time = microtime(true);
+            $result = $checker->check();
+            $time = microtime(true) - $time;
+            $result->setExecutionTime($time);
 
-			event(new HealthEvent(collect([$result])));
+            event(new HealthEvent(collect([$result])));
 
-			return $result;
+            return $result;
 
-		} catch (Exception $exception) {
-			return new Result('General Error', Result::ERROR_STATUS, $exception->getMessage(), [
-			    'line' => $exception->getLine(),
+        } catch (Exception $exception) {
+            return new Result('General Error', Result::ERROR_STATUS, $exception->getMessage(), [
+                'line' => $exception->getLine(),
                 'file' => $exception->getFile()
             ]);
-		}
-	}
+        }
+    }
 
-	public function getCheckerByName(string $name): ?array
+    public function getCheckerByName(string $name): ?array
     {
         return $this->getCheckers()[$name] ?? null;
     }
